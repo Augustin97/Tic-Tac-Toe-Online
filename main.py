41ab1590e0ada95d33928c3833464a5a4af2f0dd
Augustin Aguilar <aguilar.augustin@gmail.com>
2025-06-29 15:44:08 +0200

    fonctions logique de base du jeu

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import random
+import sys
+from tic_tac_toe import tic_tac_toe
+
+sys.path.append('../')
+
+
+if __name__ == "__main__":
+    plateau = tic_tac_toe.TicTacToe.start_game()
+    start = 0
+    list_moves = []
+    tic_tac_toe.TicTacToe.select_player(start, list_moves)
+    for i in range(0, 10):
+        print(i)
+        player = tic_tac_toe.TicTacToe.select_player(i, list_moves)
+        plateau = tic_tac_toe.TicTacToe.make_move(plateau, player)
+        tic_tac_toe.TicTacToe.check_board(plateau)
+    tic_tac_toe.TicTacToe.restart_game()
+
---
+++ tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,95 @@
+import random
+from dataclasses import dataclass
+
+@dataclass
+class TicTacToe:
+
+    @staticmethod
+    def start_game():
+        plateau = [[0,0,0],
+                   [0,0,0],
+                   [0,0,0]]
+
+        return plateau
+
+    @staticmethod
+    def select_player(turns: int, list_moves):
+        # Si la partie commence prendre un joueur au hasard
+        random_number = random.random()
+        if turns == 0:
+            if random_number > 0.5:
+                print("Player 1 starts")
+                player = "Player 1"
+                list_moves.append(player)
+            else:
+                print("Player 2 starts")
+                player = "Player 2"
+                list_moves.append(player)
+        else:
+            # vérifier le premier joueur et en fonction de si la position est pair ou pas alterner entre chaque joueur
+            first_player = list_moves[0]
+            if first_player == "Player 1":
+                if turns % 2 == 0:
+                    list_moves.append("Player 2")
+                else:
+                    list_moves.append("Player 1")
+            else:
+                if turns % 2 == 0:
+                    list_moves.append("Player 2")
+                else:
+                    list_moves.append("Player 1")
+            player = list_moves[-1]
+        return player
+
+        # Vérifier qui est le dernier à avoir joué et donner la main au suivant
+
+    @staticmethod
+    def make_move(plateau, player):
+        # Penser aux cas limites, i.e un utilisateur mets une valeur qui dépasse les bounds du tableau
+        # try except de la valeur rentrée par l'utilisateur
+        x, y = input("Veuillez rentrer la position de votre choix: " ).split()
+        if plateau[int(x)][int(y)] != 0:
+            print("Cette position est déjà prise")
+        else:
+            if player == "Player 1":
+                plateau[int(x)][int(y)] = "X"
+            else:
+                plateau[int(x)][int(y)] = "O"
+        print(plateau)
+        return plateau
+
+    @staticmethod
+    def check_board(plateau):
+        # dernier cas celui en diagonale
+        c, r = 0, 0
+        end = False
+        for row in plateau:
+            if row == ["X", "X", "X"] or row == ["O", "O", "O"]:
+                print("la partie est terminée")
+                break
+
+        buffer = []
+        while c < 3:
+            for i in range(len(plateau)):
+                buffer.append(plateau[i][c])
+            if buffer == ["X", "X", "X"] or buffer == ["O", "O", "O"]:
+                print("La partie est terminée")
+                break
+            else:
+                buffer = []
+                c += 1
+        return end
+
+    @staticmethod
+    def end_game():
+        # Une fois que le plateau est actualisé, vérifier qu'aucun des joueurs ait gagné
+        # Si un joueur gagne terminer la partie et si le tableau est rempli terminer aussi
+        # condition pour terminer une partie de renvoyer un booléen.
+        return
+
+    def restart_game(self):
+        reset = input("Voulez vous rejouer? : ")
+        if reset == "oui":
+            self.start_game()
+        else:
+            self.end_game()
